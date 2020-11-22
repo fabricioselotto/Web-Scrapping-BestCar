9fa319fc9ce62196fd63037a5b7846a695204c25
fabricioselotto <fabricioselottohard01@gmail.com>
2020-11-21 21:15:52 -0300

    Correção

--- webscrapping-icarros.py
+++ webscrapping-icarros.py
@@ -5,7 +5,7 @@ import re
 from pymongo import MongoClient
 
 # DB connectivity
-client = MongoClient("mongodb+srv://bestcar:GB0Uni0SwlsWUT3k@bestcar.kgm2k.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority")
+client = MongoClient("ENDEREÇO AQUI")
 db = client.BestCar
 collection_carros = db.carros
 
